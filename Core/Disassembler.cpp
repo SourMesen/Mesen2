982d0e00109e5a5b463e9bab16da6a3f87825465
Sour <mesenemu@gmail.com>
2019-08-04 20:10:35 -0400

    Debugger: Change solution to remove addresses on sub start headers (previous code caused crash when a label exists)

--- Core/Disassembler.cpp
+++ Core/Disassembler.cpp
@@ -252,7 +252,7 @@ void Disassembler::Disassemble(CpuType cpuType)
 			byteCounter = 0;
 
 			if(addrInfo.Type == SnesMemoryType::PrgRom && _cdl->IsSubEntryPoint(addrInfo.Address)) {
-				results.push_back(DisassemblyResult(-1, LineFlags::SubStart | LineFlags::BlockStart | LineFlags::VerifiedCode));
+				results.push_back(DisassemblyResult(addrInfo, i, LineFlags::SubStart | LineFlags::BlockStart | LineFlags::VerifiedCode));
 			}
 
 			if(_labelManager->GetLabelAndComment(addrInfo, labelInfo)) {
@@ -540,7 +540,7 @@ bool Disassembler::GetLineData(CpuType type, uint32_t lineIndex, CodeLineData &d
 			}
 
 			if(data.Flags & (LineFlags::BlockStart | LineFlags::BlockEnd)) {
-				if(!(data.Flags & LineFlags::ShowAsData)) {
+				if(!(data.Flags & (LineFlags::ShowAsData | LineFlags::SubStart))) {
 					//For hidden blocks, give the start/end lines an address
 					data.Address = result.CpuAddress;
 					data.AbsoluteAddress = result.Address.Address;
