92041510654f7cd075dad01179e67578aeba2f34
Sour <mesenemu@gmail.com>
2019-10-19 16:47:46 -0400

    UI: Fixed file filter to include SPC files

--- Core/SpcHud.cpp
+++ Core/SpcHud.cpp
@@ -18,13 +18,11 @@ void SpcHud::Draw(uint32_t frame)
 	_hud->DrawString(20, 30, "Track:", 0xBBBBBB, 0, 1, frame);
 	_hud->DrawString(20, 40, "Artist:", 0xBBBBBB, 0, 1, frame);
 	_hud->DrawString(20, 50, "Comment:", 0xBBBBBB, 0, 1, frame);
-	_hud->DrawString(20, 60, "Dumper:", 0xBBBBBB, 0, 1, frame);
 
 	_hud->DrawString(70, 20, _spcData->GameTitle, 0xFFFFFF, 0, 1, frame);
 	_hud->DrawString(70, 30, _spcData->SongTitle, 0xFFFFFF, 0, 1, frame);
 	_hud->DrawString(70, 40, _spcData->Artist, 0xFFFFFF, 0, 1, frame);
 	_hud->DrawString(70, 50, _spcData->Comment, 0xFFFFFF, 0, 1, frame);
-	_hud->DrawString(70, 60, _spcData->Dumper, 0xFFFFFF, 0, 1, frame);
 
 	int16_t left, right;
 	_mixer->GetLastSamples(left, right);
