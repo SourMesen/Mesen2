4f98329f3309ba3f646ab062ae334ecb5c6f992d
Sour <mesenemu@gmail.com>
2019-10-24 18:09:35 -0400

    UI: Allow recent game selection screen to display 4 or 9 titles based on window size

---
+++ UI/Controls/ctrlRecentGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Mesen.GUI.Emulation;
+
+namespace Mesen.GUI.Controls
+{
+	public partial class ctrlRecentGame : UserControl
+	{
+		private RecentGameInfo _recentGame;
+
+		public ctrlRecentGame()
+		{
+			InitializeComponent();
+		}
+
+		public RecentGameInfo RecentGame
+		{
+			get { return _recentGame; }
+			set
+			{
+				if(_recentGame == value) {
+					return;
+				}
+
+				_recentGame = value;
+
+				lblSaveDate.Visible = false;
+
+				if(value == null) {
+					picPreviousState.Visible = false;
+					lblGameName.Visible = false;
+					return;
+				}
+
+				lblGameName.Text = Path.GetFileNameWithoutExtension(_recentGame.RomName);
+				lblSaveDate.Text = _recentGame.Timestamp.ToString();
+
+				try {
+					ZipArchive zip = new ZipArchive(new MemoryStream(File.ReadAllBytes(_recentGame.FileName)));
+					ZipArchiveEntry entry = zip.GetEntry("Screenshot.png");
+					if(entry != null) {
+						using(Stream stream = entry.Open()) {
+							picPreviousState.Image = Image.FromStream(stream);
+						}
+					} else {
+						picPreviousState.Image = null;
+					}
+				} catch {
+					picPreviousState.Image = null;
+				}
+
+				lblGameName.Visible = true;
+				lblSaveDate.Visible = true;
+				picPreviousState.Visible = true;
+			}
+		}
+
+		public bool Highlight
+		{
+			set { picPreviousState.Highlight = value; }
+		}
+
+		private void picPreviousState_Click(object sender, EventArgs e)
+		{
+			EmuRunner.LoadRecentGame(_recentGame.FileName);
+		}
+	}
+}
--- UI/Controls/ctrlRecentGames.cs
+++ UI/Controls/ctrlRecentGames.cs
@@ -19,6 +19,9 @@ namespace Mesen.GUI.Controls
 {
 	public partial class ctrlRecentGames : BaseControl
 	{
+		private int _elementsPerRow = 0;
+		private int _elementsPerPage = 0;
+
 		public delegate void RecentGameLoadedHandler(RecentGameInfo gameInfo);
 
 		public new event MouseEventHandler MouseMove
@@ -36,7 +39,7 @@ namespace Mesen.GUI.Controls
 		private bool _initialized = false;
 		private int _currentIndex = 0;
 		private List<RecentGameInfo> _recentGames = new List<RecentGameInfo>();
-		private PrivateFontCollection _fonts = new PrivateFontCollection();
+		private List<ctrlRecentGame> _controls = new List<ctrlRecentGame>();
 
 		public ctrlRecentGames()
 		{
@@ -47,11 +50,49 @@ namespace Mesen.GUI.Controls
 
 			DoubleBuffered = true;
 
-			_fonts.AddFontFile(Path.Combine(ConfigManager.HomeFolder, "Resources", "PixelFont.ttf"));
-			lblGameName.Font = new Font(_fonts.Families[0], 10);
-			lblSaveDate.Font = new Font(_fonts.Families[0], 10);
-
 			picPrevGame.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+		}
+
+		private void InitGrid()
+		{
+			int elementsPerRow = 1;
+			if(ClientSize.Width > 850 && ClientSize.Height > 850) {
+				elementsPerRow = 3;
+			} else if(ClientSize.Width > 450 && ClientSize.Height > 450) {
+				elementsPerRow = 2;
+			}
+
+			if(_recentGames.Count <= 1) {
+				elementsPerRow = 1;
+			} else if(_recentGames.Count <= 4) {
+				elementsPerRow = Math.Min(2, elementsPerRow);
+			}
+
+			if(_elementsPerRow == elementsPerRow) {
+				return;
+			}
+
+			_elementsPerRow = elementsPerRow;
+			_elementsPerPage = elementsPerRow * elementsPerRow;
+
+			_controls = new List<ctrlRecentGame>();
+			tlpGrid.ColumnCount = _elementsPerRow;
+			tlpGrid.RowCount = _elementsPerRow;
+			tlpGrid.ColumnStyles.Clear();
+			tlpGrid.RowStyles.Clear();
+			tlpGrid.Controls.Clear();
+			for(int j = 0; j < _elementsPerRow; j++) {
+				tlpGrid.RowStyles.Add(new RowStyle(SizeType.Percent, 100F / _elementsPerRow));
+				for(int i = 0; i < _elementsPerRow; i++) {
+					tlpGrid.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F / _elementsPerRow));
+					ctrlRecentGame ctrl = new ctrlRecentGame();
+					ctrl.Dock = DockStyle.Fill;
+					ctrl.Margin = new Padding(2);
+					tlpGrid.Controls.Add(ctrl, i, j);
+					_controls.Add(ctrl);
+				}
+			}
+			UpdateGameInfo();
 		}
 
 		public new bool Visible
@@ -68,7 +109,7 @@ namespace Mesen.GUI.Controls
 						//We just re-enabled the screen, initialize it
 						Initialize();
 					}
-
+					InitGrid();
 					base.Visible = value;
 					tmrInput.Enabled = value;
 				}
@@ -82,6 +123,8 @@ namespace Mesen.GUI.Controls
 
 		public void Initialize()
 		{
+			InitGrid();
+
 			_initialized = true;
 			_recentGames = new List<RecentGameInfo>();
 			_currentIndex = 0;
@@ -107,8 +150,8 @@ namespace Mesen.GUI.Controls
 
 			_recentGames = _recentGames.OrderBy((info) => info.Timestamp).Reverse().ToList();
 
-			if(_recentGames.Count > 5) {
-				_recentGames.RemoveRange(5, _recentGames.Count - 5);
+			if(_recentGames.Count > 20) {
+				_recentGames.RemoveRange(36, _recentGames.Count - 36);
 			}
 
 			if(_recentGames.Count == 0) {
@@ -119,33 +162,19 @@ namespace Mesen.GUI.Controls
 				tmrInput.Enabled = true;
 			}
 
-			picPreviousState.Visible = _recentGames.Count > 0;
-			picPrevGame.Visible = _recentGames.Count > 1;
-			picNextGame.Visible = _recentGames.Count > 1;
+			picPrevGame.Visible = true;
+			picNextGame.Visible = true;
 		}
 
 		public void UpdateGameInfo()
 		{
-			if(_currentIndex < _recentGames.Count) {
-				lblGameName.Text = Path.GetFileNameWithoutExtension(_recentGames[_currentIndex].RomName);
-				lblSaveDate.Text = _recentGames[_currentIndex].Timestamp.ToString();
+			int count = _recentGames.Count;
+			int pageStart = _currentIndex / _elementsPerPage * _elementsPerPage;
 
-				try {
-					ZipArchive zip = new ZipArchive(new MemoryStream(File.ReadAllBytes(_recentGames[_currentIndex].FileName)));
-					ZipArchiveEntry entry = zip.GetEntry("Screenshot.png");
-					if(entry != null) {
-						using(Stream stream = entry.Open()) {
-							picPreviousState.Image = Image.FromStream(stream);
-						}
-					} else {
-						picPreviousState.Image = null;
-					}
-				} catch {
-					picPreviousState.Image = null;
-				}
-				UpdateSize();
+			for(int i = 0; i < _elementsPerPage; i++) {
+				_controls[i].RecentGame = count > pageStart + i ? _recentGames[pageStart+i] : null;
+				_controls[i].Highlight = (_currentIndex % _elementsPerPage) == i;
 			}
-			picPreviousState.Visible = _recentGames.Count > 0;
 		}
 
 		float _xFactor = 1;
@@ -155,22 +184,6 @@ namespace Mesen.GUI.Controls
 			_xFactor = factor.Width;
 			_yFactor = factor.Height;
 			base.ScaleControl(factor, specified);
-		}
-
-		private void UpdateSize()
-		{
-			tlpPreviousState.Visible = false;
-			Size maxSize = new Size(this.Size.Width - (int)(120 * _xFactor), this.Size.Height - (int)(50 * _yFactor));
-
-			if(picPreviousState.Image != null) {
-				double xRatio = (double)picPreviousState.Image.Width / maxSize.Width;
-				double yRatio = (double)picPreviousState.Image.Height / maxSize.Height;
-				double ratio = Math.Max(xRatio, yRatio);
-
-				Size newSize = new Size((int)(picPreviousState.Image.Width / ratio), (int)(picPreviousState.Image.Height / ratio));
-				picPreviousState.Size = newSize;
-			}
-			tlpPreviousState.Visible = true;
 		}
 
 		protected override void OnResize(EventArgs e)
@@ -183,46 +196,38 @@ namespace Mesen.GUI.Controls
 				picPrevGame.Dock = DockStyle.Fill;
 			}
 
-			if(picPreviousState.Image != null) {
-				UpdateSize();
-			}
+			InitGrid();
 			base.OnResize(e);
-		}
-		
-		private void picPreviousState_Click(object sender, EventArgs e)
-		{
-			LoadSelectedGame();
 		}
 
 		private void picNextGame_MouseDown(object sender, MouseEventArgs e)
 		{
-			GoToNextGame();
+			GoToNextPage();
 		}
 
 		private void picPrevGame_MouseDown(object sender, MouseEventArgs e)
 		{
-			GoToPreviousGame();
+			GoToPreviousPage();
 		}
 
-		private void GoToPreviousGame()
+		private void GoToPreviousPage()
 		{
-			if(_currentIndex == 0) {
-				_currentIndex = _recentGames.Count - 1;
+			if(_currentIndex < _elementsPerPage) {
+				_currentIndex = _recentGames.Count - _elementsPerPage;
 			} else {
-				_currentIndex--;
+				_currentIndex -= _elementsPerPage;
 			}
 			UpdateGameInfo();
 		}
 
-		private void GoToNextGame()
+		private void GoToNextPage()
 		{
-			_currentIndex = (_currentIndex + 1) % _recentGames.Count;
+			if(_currentIndex + _elementsPerPage < _recentGames.Count) {
+				_currentIndex += _elementsPerPage;
+			} else {
+				_currentIndex = 0;
+			}
 			UpdateGameInfo();
-		}
-
-		private void LoadSelectedGame()
-		{
-			EmuRunner.LoadRecentGame(_recentGames[_currentIndex].FileName);
 		}
 
 		private bool _waitForRelease = false;
@@ -244,13 +249,51 @@ namespace Mesen.GUI.Controls
 						foreach(KeyMapping mapping in mappings) {
 							if(mapping.Left == keyCode) {
 								_waitForRelease = true;
-								GoToPreviousGame();
+								if(_currentIndex == 0) {
+									_currentIndex = _recentGames.Count - 1;
+								} else {
+									_currentIndex--;
+								}
+								UpdateGameInfo();
 							} else if(mapping.Right == keyCode) {
 								_waitForRelease = true;
-								GoToNextGame();
-							} else if(mapping.A == keyCode || mapping.B == keyCode) {
+								_currentIndex = (_currentIndex + 1) % _recentGames.Count;
+								UpdateGameInfo();
+							} else if(mapping.Down == keyCode) {
 								_waitForRelease = true;
-								LoadSelectedGame();
+								if(_currentIndex + _elementsPerRow < _recentGames.Count) {
+									_currentIndex += _elementsPerRow;
+								} else {
+									_currentIndex = 0;
+								}
+								UpdateGameInfo();
+							} else if(mapping.Up == keyCode) {
+								_waitForRelease = true;
+								if(_currentIndex < _elementsPerRow) {
+									_currentIndex = _recentGames.Count - _elementsPerRow;
+								} else {
+									_currentIndex -= _elementsPerRow;
+								}
+								UpdateGameInfo();
+							} else if(mapping.L == keyCode) {
+								_waitForRelease = true;
+								if(_currentIndex < _elementsPerPage) {
+									_currentIndex = _recentGames.Count - _elementsPerPage;
+								} else {
+									_currentIndex -= _elementsPerPage;
+								}
+								UpdateGameInfo();
+							} else if(mapping.R == keyCode) {
+								_waitForRelease = true;
+								if(_currentIndex + _elementsPerPage < _recentGames.Count) {
+									_currentIndex += _elementsPerPage;
+								} else {
+									_currentIndex = 0; ;
+								}
+								UpdateGameInfo();
+							} else if(mapping.A == keyCode || mapping.B == keyCode || mapping.X == keyCode || mapping.Y == keyCode || mapping.Select == keyCode || mapping.Start == keyCode) {
+								_waitForRelease = true;
+								EmuRunner.LoadRecentGame(_recentGames[_currentIndex].FileName);
 							}
 						}
 					}
@@ -273,11 +316,22 @@ namespace Mesen.GUI.Controls
 	{
 		public System.Drawing.Drawing2D.InterpolationMode InterpolationMode { get; set; }
 		private bool _hovered = false;
+		private bool _highlight = false;
 
 		public GamePreviewBox()
 		{
 			DoubleBuffered = true;
 			InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Default;
+		}
+
+		public bool Highlight
+		{
+			get { return _highlight; }
+			set
+			{
+				_highlight = value;
+				this.Invalidate();
+			}
 		}
 
 		protected override void OnMouseEnter(EventArgs e)
@@ -299,7 +353,7 @@ namespace Mesen.GUI.Controls
 			pe.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
 			base.OnPaint(pe);
 
-			using(Pen pen = new Pen(_hovered ? Color.LightBlue : Color.Gray, 2)) {
+			using(Pen pen = new Pen(_hovered ? Color.LightBlue : (_highlight ? Color.Orange : Color.Gray), 2)) {
 				pe.Graphics.DrawRectangle(pen, 1, 1, this.Width - 2, this.Height - 2);
 			}
 		}
