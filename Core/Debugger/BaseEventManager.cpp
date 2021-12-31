e6a79f01333326acf67c12e72a9688be6e2dc784
Sour <mesenemu@gmail.com>
2021-12-30 22:29:15 -0500

    Event Viewer: Fixed behavior on cycle 0

--- Core/Debugger/BaseEventManager.cpp
+++ Core/Debugger/BaseEventManager.cpp
@@ -6,7 +6,7 @@ void BaseEventManager::FilterEvents()
 	auto lock = _lock.AcquireSafe();
 	_sentEvents.clear();
 
-	if(ShowPreviousFrameEvents() && _snapshotScanline != 0 && _snapshotCycle != 0) {
+	if(ShowPreviousFrameEvents() && (_snapshotScanline != 0 || _snapshotCycle != 0)) {
 		int offset = GetScanlineOffset();
 		uint32_t key = (_snapshotScanline << 16) + _snapshotCycle;
 		for(DebugEventInfo& evt : _snapshotPrevFrame) {
