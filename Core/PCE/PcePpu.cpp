c4395fdc01edae1970078c65bce48d8fa54ffbdb
Sour <mesenemu@gmail.com>
2022-04-11 00:46:48 -0400

    Fixed PPU vram addr inc init value

--- Core/PCE/PcePpu.cpp
+++ Core/PCE/PcePpu.cpp
@@ -23,9 +23,10 @@ PcePpu::PcePpu(Emulator* emu, PceConsole* console)
 	memset(_vram, 0, 0x10000);
 	memset(_paletteRam, 0, 0x400);
 
-	//Column/Row count can't ever be 0, init them
+	//These values can't ever be 0, init them to a possible value
 	_state.ColumnCount = 32;
 	_state.RowCount = 32;
+	_state.VramAddrIncrement = 1;
 
 	_emu->RegisterMemory(MemoryType::PceVideoRam, _vram, 0x8000 * sizeof(uint16_t));
 	_emu->RegisterMemory(MemoryType::PcePaletteRam, _paletteRam, 0x200 * sizeof(uint16_t));
