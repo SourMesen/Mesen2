b620d6bece564374d2d7c1da8fef6a6765177c8f
Sour <mesenemu@gmail.com>
2022-04-17 12:56:12 -0400

    PGO support for PCE games

--- PGOHelper/PGOHelper.cpp
+++ PGOHelper/PGOHelper.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
 		romFolder = argv[1];
 	}
 
-	vector<string> testRoms = GetFilesInFolder(romFolder, { {".sfc", ".gb", ".gbc", ".nes"} });
+	vector<string> testRoms = GetFilesInFolder(romFolder, { ".sfc", ".gb", ".gbc", ".nes", ".pce" });
 	PgoRunTest(testRoms, true);
 	return 0;
 }
