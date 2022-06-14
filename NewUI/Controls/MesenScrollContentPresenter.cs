ffd4bec38d53c62dd2cebe4c81cf7bf64154ec07
Sour <mesenemu@gmail.com>
2022-06-14 00:56:59 -0400

    UI: Fixed performance issues with DataBox in some scenarios

--- NewUI/Controls/MesenScrollContentPresenter.cs
+++ NewUI/Controls/MesenScrollContentPresenter.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Presenters;
 using Avalonia.Input;
+using Avalonia.LogicalTree;
 using Avalonia.Styling;
+using DataBoxControl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +16,21 @@ namespace Mesen.Controls
 	public class MesenScrollContentPresenter : ScrollContentPresenter, IStyleable
 	{
 		Type IStyleable.StyleKey => typeof(ScrollContentPresenter);
+		
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			if(this.FindLogicalAncestorOfType<DataBox>() != null) {
+				//When inside a DataBox, use availableSize directly without
+				//calling MeasureOverride on the ScrollContentPresenter
+				//Otherwise, the DataBox tries to resize with the height set
+				//to the screen's height, causing a large number of rows to
+				//be created, and then removed again once the measure is
+				//re-calculated with the correct sizes.
+				Child.Measure(availableSize);
+				return Child.DesiredSize.Constrain(availableSize);
+			}
+			return base.MeasureOverride(availableSize);
+		}
 
 		protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
 		{
