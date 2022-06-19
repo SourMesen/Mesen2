ad6c9c423eb218c390d96a3dc967d9009734139f
Sour <mesenemu@gmail.com>
2022-06-19 17:18:40 -0400

    UI: Remove patch to allow shift+wheel to scroll horizontally (seems to be supported natively by Avalonia now)

--- NewUI/Controls/MesenScrollContentPresenter.cs
+++ NewUI/Controls/MesenScrollContentPresenter.cs
@@ -34,11 +34,6 @@ namespace Mesen.Controls
 
 		protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
 		{
-			if(e.KeyModifiers == KeyModifiers.Shift) {
-				//Allow shift+wheel for horizontal scrolling
-				e.Delta = new Avalonia.Vector(e.Delta.Y, e.Delta.X);
-			}
-
 			if(!e.KeyModifiers.HasFlag(KeyModifiers.Control)) {
 				//Skip event if control is pressed, because this is used to zoom in/out
 				base.OnPointerWheelChanged(e);
