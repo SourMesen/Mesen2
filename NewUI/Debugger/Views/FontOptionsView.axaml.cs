7b6c0741c6e26041b8a914f59ea43531dee1bd31
Sour <mesenemu@gmail.com>
2022-08-14 18:29:07 -0400

    Debugger: Improve debug settings dialog startup performance, improve validations for selected font (but allow non-monospace fonts)

--- NewUI/Debugger/Views/FontOptionsView.axaml.cs
+++ NewUI/Debugger/Views/FontOptionsView.axaml.cs
@@ -3,27 +3,72 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Mesen.Config;
+using Mesen.Debugger.Controls;
+using Mesen.Utilities;
+using Mesen.Windows;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Mesen.Debugger.Views
 {
 	public class FontOptionsView : UserControl
 	{
+		public static readonly StyledProperty<string> InternalFontFamilyProperty = AvaloniaProperty.Register<FontOptionsView, string>(nameof(InternalFontFamily), "", defaultBindingMode: Avalonia.Data.BindingMode.TwoWay);
+
+		public string InternalFontFamily
+		{
+			get { return GetValue(InternalFontFamilyProperty); }
+			set { SetValue(InternalFontFamilyProperty, value); }
+		}
+
+		static FontOptionsView()
+		{
+			InternalFontFamilyProperty.Changed.AddClassHandler<FontOptionsView>((s, e) => s.ValidateFont());
+		}
+
 		public FontOptionsView()
 		{
 			InitializeComponent();
 
 			ComboBox cboFontFamily = this.GetControl<ComboBox>("cboFontFamily");
-			List<string> fontFamilies = FontManager.Current.GetInstalledFontFamilyNames().Where((family) => {
-				return new Typeface(new FontFamily(family)).GlyphTypeface.IsFixedPitch;
-			}).ToList();
+			List<string> fontFamilies = FontManager.Current.GetInstalledFontFamilyNames().ToList();
 			fontFamilies.Sort();
 			cboFontFamily.Items = fontFamilies;
 
 			ComboBox cboFontSize = this.GetControl<ComboBox>("cboFontSize");
 			cboFontSize.Items = new float[] { 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18 };
+		}
+
+		protected override void OnDataContextChanged(EventArgs e)
+		{
+			base.OnDataContextChanged(e);
+			if(DataContext is FontConfig cfg) {
+				InternalFontFamily = cfg.FontFamily;
+			}
+		}
+
+		private async void ValidateFont()
+		{
+			if(DataContext is FontConfig cfg) {
+				Typeface typeface = new Typeface(new FontFamily(InternalFontFamily));
+				if(!typeface.GlyphTypeface.IsFixedPitch) {
+					if(await MesenMsgBox.Show(VisualRoot, "NonMonospaceFontWarning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, InternalFontFamily) != Mesen.Windows.DialogResult.Yes) {
+						InternalFontFamily = cfg.FontFamily;
+						return;
+					}
+				}
+
+				FormattedText text = new FormattedText("W", CultureInfo.CurrentCulture, FlowDirection.LeftToRight, typeface, 10, null);
+				if(text.Width < 2 || text.Height < 2) {
+					await MesenMsgBox.Show(VisualRoot, "InvalidFont", MessageBoxButtons.OK, MessageBoxIcon.Error, InternalFontFamily);
+					InternalFontFamily = cfg.FontFamily;
+					return;
+				}
+
+				cfg.FontFamily = InternalFontFamily;
+			}
 		}
 
 		private void InitializeComponent()
