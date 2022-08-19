3f7391343644b9b99ac376c4e9f42c5971d794aa
Sour <mesenemu@gmail.com>
2022-08-18 23:53:20 -0400

    UI: Increase wheel scroll speed for lists

--- NewUI/ThirdParty/DataBox/Primitives/DataBoxRowsPresenter.cs
+++ NewUI/ThirdParty/DataBox/Primitives/DataBoxRowsPresenter.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Generators;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Styling;
+using Avalonia.VisualTree;
 
 namespace DataBoxControl.Primitives;
 
@@ -11,7 +16,31 @@ public class DataBoxRowsPresenter : ListBox, IStyleable
 
     Type IStyleable.StyleKey => typeof(DataBoxRowsPresenter);
 
-    protected override IItemContainerGenerator CreateItemContainerGenerator()
+	protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+	{
+		base.OnApplyTemplate(e);
+		ScrollViewer? viewer = this.FindDescendantOfType<ScrollViewer>();
+		if(viewer != null) {
+			viewer.AddHandler<PointerWheelEventArgs>(ScrollViewer.PointerWheelChangedEvent, DataBoxRowsPresenter_PointerWheelChanged, RoutingStrategies.Tunnel);
+		}
+	}
+
+	private void DataBoxRowsPresenter_PointerWheelChanged(object? sender, PointerWheelEventArgs e)
+	{
+		if(sender is ScrollViewer viewer && e.KeyModifiers != KeyModifiers.Shift) {
+			double offset = viewer.Offset.Y - e.Delta.Y * 3;
+			if(offset < 0) {
+				viewer.Offset = viewer.Offset.WithY(0);
+			} else if(offset > viewer.Extent.Height) {
+				viewer.Offset = viewer.Offset.WithY(viewer.Extent.Height);
+			} else {
+				viewer.Offset = viewer.Offset.WithY(offset);
+			}
+			e.Handled = true;
+		}
+	}
+
+	protected override IItemContainerGenerator CreateItemContainerGenerator()
     {
         var generator = new ItemContainerGenerator<DataBoxRow>(
             this,
