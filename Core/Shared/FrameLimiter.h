42ff564ad9a1827f3a0e6424f72bb70855026270
Sour <mesenemu@gmail.com>
2022-10-15 13:20:24 -0400

    Emulation: Lowered 300ms check to 100ms to prevent emulator from trying to catch up if pausing+unpausing within 0.3 secs

--- Core/Shared/FrameLimiter.h
+++ Core/Shared/FrameLimiter.h
@@ -25,11 +25,11 @@ public:
 
 	void ProcessFrame()
 	{
-		if(_resetRunTimers || (_clockTimer.GetElapsedMS() - _targetTime) > 300) {
+		if(_resetRunTimers || (_clockTimer.GetElapsedMS() - _targetTime) > 100) {
 			//Reset the timers, this can happen in 3 scenarios:
 			//1) Target frame rate changed
 			//2) The console was reset/power cycled or the emulation was paused (with or without the debugger)
-			//3) As a satefy net, if we overshoot our target by over 300 milliseconds, the timer is reset, too.
+			//3) As a satefy net, if we overshoot our target by over 100 milliseconds, the timer is reset, too.
 			//   This can happen when something slows the emulator down severely (or when breaking execution in VS when debugging Mesen itself, etc.)
 			_clockTimer.Reset();
 			_targetTime = 0;
