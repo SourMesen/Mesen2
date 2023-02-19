2bab0273b01a8ea7aacee05fff1b913cc92cc1b7
Sour <mesenemu@gmail.com>
2023-02-19 12:08:26 -0500

    UI: Add low latency warning in audio config

--- UI/ViewModels/AudioConfigViewModel.cs
+++ UI/ViewModels/AudioConfigViewModel.cs
@@ -2,8 +2,11 @@
 using Mesen.Config;
 using Mesen.Interop;
 using Mesen.Utilities;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System.Reactive.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace Mesen.ViewModels
 {
@@ -12,7 +15,8 @@ namespace Mesen.ViewModels
 		[Reactive] public AudioConfig Config { get; set; }
 		[Reactive] public AudioConfig OriginalConfig { get; set; }
 		[Reactive] public List<string> AudioDevices { get; set; } = new();
-		
+		[Reactive] public bool ShowLatencyWarning { get; set; } = false;
+
 		public AudioConfigViewModel()
 		{
 			Config = ConfigManager.Config.Audio;
@@ -26,6 +30,10 @@ namespace Mesen.ViewModels
 			if(AudioDevices.Count > 0 && !AudioDevices.Contains(Config.AudioDevice)) {
 				Config.AudioDevice = AudioDevices[0];
 			}
+
+			AddDisposable(this.WhenAnyValue(x => x.Config.AudioLatency).Subscribe(x => {
+				ShowLatencyWarning = Config.AudioLatency <= 55;
+			}));
 
 			AddDisposable(ReactiveHelper.RegisterRecursiveObserver(Config, (s, e) => { Config.ApplyConfig(); }));
 		}
