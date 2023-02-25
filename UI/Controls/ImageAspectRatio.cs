25eda2a8b4a1da23ffdc5d18af2fd0e8bead05ed
Sour <mesenemu@gmail.com>
2023-02-25 10:59:42 -0500

    UI: Fixed aspect ratio for thumbnails on game selection screen

---
+++ UI/Controls/ImageAspectRatio.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Controls;
+using System;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
+using Avalonia.Input;
+using Mesen.Debugger.Utilities;
+using System.Globalization;
+
+namespace Mesen.Controls
+{
+	public class ImageAspectRatio : Image, IStyleable
+	{
+		Type IStyleable.StyleKey => typeof(Image);
+
+		public ImageAspectRatio()
+		{
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			finalSize = base.ArrangeOverride(finalSize);
+			if(finalSize.Width >= finalSize.Height * 256 / 240) {
+				return new Size(finalSize.Height * 256 / 240, finalSize.Height);
+			} else {
+				return new Size(finalSize.Width, finalSize.Width * 240 / 256);
+			}
+		}
+	}
+}
