78d13506c5a1bb181b570b92f006174af6b4bb5c
Sour <mesenemu@gmail.com>
2023-04-07 20:47:31 -0400

    UI: Catch Avalonia crash when trying to load a WSL file using the "\\wsl$\" path format

--- UI/Utilities/FileDialogHelper.cs
+++ UI/Utilities/FileDialogHelper.cs
@@ -43,21 +43,22 @@ namespace Mesen.Utilities
 				throw new Exception("Invalid parent window");
 			}
 
-			List<FilePickerFileType> filter = new List<FilePickerFileType>();
-			foreach(string ext in extensions) {
-				if(ext == FileDialogHelper.RomExt) {
-					filter.Add(new FilePickerFileType("All ROM files") { Patterns = new List<string>() { "*.sfc", "*.fig", "*.smc", "*.spc", "*.nes", "*.fds", "*.unif", "*.unf", "*.studybox", "*.nsf", "*.nsfe", "*.gb", "*.gbc", "*.gbs", "*.pce", "*.sgx", "*.cue", "*.hes", "*.zip", "*.7z" } });
-					filter.Add(new FilePickerFileType("SNES ROM files") { Patterns = new List<string>() { "*.sfc", "*.fig", "*.smc", "*.spc" } });
-					filter.Add(new FilePickerFileType("NES ROM files") { Patterns = new List<string>() { "*.nes", "*.fds", "*.unif", "*.unf", "*.studybox", "*.nsf", "*.nsfe" } });
-					filter.Add(new FilePickerFileType("GB ROM files") { Patterns = new List<string>() { "*.gb", "*.gbc", "*.gbs" } });
-					filter.Add(new FilePickerFileType("PC Engine ROM files") { Patterns = new List<string>() { "*.pce", "*.sgx", "*.cue", ".hes" } });
-				} else if(ext == FileDialogHelper.FirmwareExt) {
-					filter.Add(new FilePickerFileType("All firmware files") { Patterns = new List<string>() { "*.sfc", "*.pce", "*.nes", "*.bin", "*.rom" } });
-				} else {
-					filter.Add(new FilePickerFileType(ext.ToUpper() + " files") { Patterns = new List<string>() { "*." + ext } });
+			try {
+				List<FilePickerFileType> filter = new List<FilePickerFileType>();
+				foreach(string ext in extensions) {
+					if(ext == FileDialogHelper.RomExt) {
+						filter.Add(new FilePickerFileType("All ROM files") { Patterns = new List<string>() { "*.sfc", "*.fig", "*.smc", "*.spc", "*.nes", "*.fds", "*.unif", "*.unf", "*.studybox", "*.nsf", "*.nsfe", "*.gb", "*.gbc", "*.gbs", "*.pce", "*.sgx", "*.cue", "*.hes", "*.zip", "*.7z" } });
+						filter.Add(new FilePickerFileType("SNES ROM files") { Patterns = new List<string>() { "*.sfc", "*.fig", "*.smc", "*.spc" } });
+						filter.Add(new FilePickerFileType("NES ROM files") { Patterns = new List<string>() { "*.nes", "*.fds", "*.unif", "*.unf", "*.studybox", "*.nsf", "*.nsfe" } });
+						filter.Add(new FilePickerFileType("GB ROM files") { Patterns = new List<string>() { "*.gb", "*.gbc", "*.gbs" } });
+						filter.Add(new FilePickerFileType("PC Engine ROM files") { Patterns = new List<string>() { "*.pce", "*.sgx", "*.cue", ".hes" } });
+					} else if(ext == FileDialogHelper.FirmwareExt) {
+						filter.Add(new FilePickerFileType("All firmware files") { Patterns = new List<string>() { "*.sfc", "*.pce", "*.nes", "*.bin", "*.rom" } });
+					} else {
+						filter.Add(new FilePickerFileType(ext.ToUpper() + " files") { Patterns = new List<string>() { "*." + ext } });
+					}
 				}
-			}
-			filter.Add(new FilePickerFileType("All files") { Patterns = new List<string>() { "*" } });
+				filter.Add(new FilePickerFileType("All files") { Patterns = new List<string>() { "*" } });
 
 			IReadOnlyList<IStorageFile> files = await wnd.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions() {
 				SuggestedStartLocation = initialFolder != null ? await wnd.StorageProvider.TryGetFolderFromPath(initialFolder) : null,
@@ -65,8 +66,11 @@ namespace Mesen.Utilities
 				FileTypeFilter = filter
 			});
 
-			if(files.Count > 0) {
-				return files[0].Path.LocalPath;
+				if(files.Count > 0) {
+					return files[0].Path.LocalPath;
+				}
+			} catch(Exception ex) {
+				await MesenMsgBox.ShowException(ex);
 			}
 			return null;
 		}
@@ -77,11 +81,12 @@ namespace Mesen.Utilities
 				throw new Exception("Invalid parent window");
 			}
 
-			List<FilePickerFileType> filter = new List<FilePickerFileType>();
-			foreach(string ext in extensions) {
-				filter.Add(new FilePickerFileType(ext.ToUpper() + " files") { Patterns = new List<string>() { "*." + ext } });
-			}
-			filter.Add(new FilePickerFileType("All files") { Patterns = new List<string>() { "*" } });
+			try {
+				List<FilePickerFileType> filter = new List<FilePickerFileType>();
+				foreach(string ext in extensions) {
+					filter.Add(new FilePickerFileType(ext.ToUpper() + " files") { Patterns = new List<string>() { "*." + ext } });
+				}
+				filter.Add(new FilePickerFileType("All files") { Patterns = new List<string>() { "*" } });
 
 			IStorageFile? file = await wnd.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions() {
 				SuggestedStartLocation = initialFolder != null ? await wnd.StorageProvider.TryGetFolderFromPath(initialFolder) : null,
@@ -91,8 +96,11 @@ namespace Mesen.Utilities
 				FileTypeChoices = filter
 			});
 
-			if(file != null) {
-				return file.Path.LocalPath;
+				if(file != null) {
+					return file.Path.LocalPath;
+				}
+			} catch(Exception ex) {
+				await MesenMsgBox.ShowException(ex);
 			}
 			return null;
 		}
@@ -103,12 +111,16 @@ namespace Mesen.Utilities
 				throw new Exception("Invalid parent window");
 			}
 
-			IReadOnlyList<IStorageFolder> folders = await wnd.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions() {
-				AllowMultiple = false
-			});
+			try {
+				IReadOnlyList<IStorageFolder> folders = await wnd.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions() {
+					AllowMultiple = false
+				});
 
-			if(folders.Count > 0) {
-				return folders[0].Path.LocalPath;
+				if(folders.Count > 0) {
+					return folders[0].Path.LocalPath;
+				}
+			} catch(Exception ex) {
+				await MesenMsgBox.ShowException(ex);
 			}
 			return null;
 		}
