28b75c25100ce9de48619f6f19af48d5f7ef133c
Sour <mesenemu@gmail.com>
2024-11-08 15:45:32 +0900

    UI: Fixed crashes that could occur when using undo after typing in a text box

---
+++ UI/Utilities/NullTextConverter.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using System;
+
+namespace Mesen.Utilities;
+
+/// <summary>
+/// Used to fix an issue with undo in Avalonia's TextBox.
+/// Typing a string, then undoing multiple times will eventually set
+/// the string to null instead of an empty string, causing crashes
+/// in code that does not expect the string to ever be null.
+/// </summary>
+public class NullTextConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+	{
+		if(value is string val) {
+			return val ?? "";
+		}
+		return "";
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+	{
+		if(value is string s) {
+			return s;
+		}
+		return "";
+	}
+}
