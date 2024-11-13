54b8973c613fd0699f2160a155f4a9219c0a24eb
Sour <mesenemu@gmail.com>
2024-11-13 21:25:30 +0900

    Debugger: WS - Fixed code highlighting issue caused by previous commit

--- UI/Debugger/Disassembly/CodeHighlighting.cs
+++ UI/Debugger/Disassembly/CodeHighlighting.cs
@@ -12,7 +12,7 @@ namespace Mesen.Debugger.Disassembly
 {
 	class CodeHighlighting
 	{
-		private static Regex _labelDef = new Regex("^(.+)\\s*[:=]", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+		private static Regex _labelDef = new Regex("^([^\\s]+)\\s*[:=]", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
 		private static Regex _space = new Regex("^[ \t]+", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 		private static Regex _comment = new Regex("^;.*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
