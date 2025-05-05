SourMesen/Mesen2

commit 32ab5cfa7ee64f182f5ce355afdd4afb700d648e

GBA: Fixed open bus issues This fixes a freeze in Zelda Minish Cap (in the final dungeon) and fixes a test in the mGBA test suite (Misc -> DMA prefetch -> Read)

Core/GBA/GbaMemoryManager.cpp
@@ -244,11 +244,19 @@ void GbaMemoryManager::UpdateOpenBus(uint32_t addr, uint32_t value)
 			value >>= 8;
 		}
 		memcpy(_state.InternalOpenBus, _state.IwramOpenBus, sizeof(_state.IwramOpenBus));
-	} else {
-		for(int i = 0; i < width; i++) {
-			_state.InternalOpenBus[i] = value;
-			value >>= 8;
-		}
+	} else if constexpr(width == 4) {
+		_state.InternalOpenBus[0] = value;
+		_state.InternalOpenBus[1] = value >> 8;
+		_state.InternalOpenBus[2] = value >> 16;
+		_state.InternalOpenBus[3] = value >> 24;
+	} else if constexpr(width == 2) {
+		_state.InternalOpenBus[2] = _state.InternalOpenBus[0] = value;
+		_state.InternalOpenBus[3] = _state.InternalOpenBus[1] = value >> 8;
+	} else if constexpr(width == 1) {
+		_state.InternalOpenBus[0] = value;
+		_state.InternalOpenBus[1] = value;
+		_state.InternalOpenBus[2] = value;
+		_state.InternalOpenBus[3] = value;
 	}
 }
 

commit f1256c840d113bc00e566cf9c86992d22b8547f5

GBA: Fixed regression that caused the test rom to dump the bios to save ram to fail This was broken by 32ab5cfa7ee64f182f5ce355afdd4afb700d648e Reading from open bus should probably not affect open bus, since no device is putting any data on the bus in this case

Core/GBA/GbaMemoryManager.cpp
@@ -289,6 +289,13 @@ void GbaMemoryManager::ProcessVramStalling(uint8_t memType)
 template<uint8_t width>
 void GbaMemoryManager::UpdateOpenBus(uint32_t addr, uint32_t value)
 {
+	if(addr >= 0x10000000) {
+		//Accessing open bus addresses should probably not update the current open bus value
+		//This is needed to pass the test rom that dumps the bios to save ram by abusing open bus (See: https://gist.github.com/profi200/c7fef99003fa5d07235d97296da23db3)
+		//TODOGBA reading other open bus addresses probably should behave the same? (e.g registers that don't exist, etc.)
+		return;
+	}
+
 	if((addr & 0xFF000000) == 0x03000000) {
 		//IWRAM appears to have its own open bus value, which overwrites
 		//the main bus' open bus value whenever IWRAM is read